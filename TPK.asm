f3ecc168779c470388ca7b0ca0963fd2c94d17d6
JSM <jtech1995@gmail.com>
2015-12-02 22:27:29 -0500

    template in place, compiled

--- TPK.asm
+++ TPK.asm
@@ -1,1 +1,22 @@
-Hello
+;       Program: Boot-loader and TPK - Timer Pre-emptive Kernel
+;   Description: Bootstraps the computer to a small OS
+;        Author: Jared Messer and Mark Bixler
+;          Date: 
+;         Notes: 
+; Help Received: 
+
+; Is all this right?
+.model small
+.386
+.stack 100h
+
+.data
+
+.code
+main proc
+    mov  ax, @data          ; @data is the data segment that DOS sets up.
+    mov  ds, ax             ; These two lines are required for all programs
+
+main endp
+
+end main
