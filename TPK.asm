c8f950eea54b2f138bc3a4b7bbcebfa5ab5ef615
JSM <jtech1995@gmail.com>
2015-12-02 23:51:27 -0500

    adding an outline

--- TPK.asm
+++ TPK.asm
@@ -16,7 +16,24 @@
 main proc
     mov  ax, @data          ; @data is the data segment that DOS sets up.
     mov  ds, ax             ; These two lines are required for all programs
+    
+    ;BOOTLOADER
+    ;*******************************************************
+    ; Insert MBR signature
+    ; Load Kernel from sectors 2 to whatever our kernel uses
+    ; to mem location 0800:0000
+    ; Prompt for key press
+    ; Start executing
+    ;*******************************************************
+    
+    ;TPK
+    ;*******************************************************
+    ;
+    ;
+    ;
+    ;*******************************************************
 
+    ;exit
 main endp
 
 end main
