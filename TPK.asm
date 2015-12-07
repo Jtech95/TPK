7a14e1eeeb0336956996cc67216c89417741afaa
Mark Bixler <MarkBixler@ymail.com>
2015-12-07 08:34:31 -0500

    Wrote kernal as simple hello world

--- TPK.asm
+++ TPK.asm
@@ -6,48 +6,49 @@
 ; Help Received: 
 
 ; Mr. J, does this need to be stated twice?
-; .model tiny
-; .386
-; .stack 100h
+ .model tiny
+ .386
+ .stack 100h
 
 .data
 TASK_STACKS	equ	SOME ADDRESS
 SP_ARRAY	equ	SOME ADDRESS
 counter		byte	0
+message 	byte	"Hello World!!", 0
 .code
 
-task1 proc
-	push	ax
-	mov	al, '|'
-	mov	es:[0], al
-	mov	al, '/'
-	mov	es:[0], al
-	mov	al, '-'
-	mov	es:[0], al
-	mov	al, '\'
-	mov	es:[0], al
-	pop	ax
-task1 endp
+;task1 proc
+;	push	ax
+;	mov	al, '|'
+;	mov	es:[0], al
+;	mov	al, '/'
+;	mov	es:[0], al
+;	mov	al, '-'
+;	mov	es:[0], al
+;	mov	al, '\'
+;	mov	es:[0], al
+;	pop	ax
+;task1 endp
 
-task2 proc
-	push	ax
-	mov	al, '>'
-	mov	es:[counter], al
-	inc	[counter]
-	cmp	[counter], 80
-	je	reset_counter
-	jmp	after_reset_counter
-reset_counter:
-	xor	[counter], [counter]
-after_reset_counter:
-	pop	ax
-task2 endp
+;task2 proc
+;	push	ax
+;	mov	al, '>'
+;	mov	es:[counter], al
+;	inc	[counter]
+;	cmp	[counter], 80
+;	je	reset_counter
+;	jmp	after_reset_counter
+;reset_counter:
+;	xor	[counter], [counter]
+;after_reset_counter:
+;	pop	ax
+;task2 endp
 
 task3 proc
-task4 endp
+task3 endp
 
 task4 proc
-task5 endp
+task4 endp
 
 task5 proc
 task5 endp
@@ -55,6 +56,29 @@ task5 endp
 task6 proc
 task6 endp
 
+; Function: prints a NUL-terminated string
+; Receives: DX=offset of string (in DS)
+; Returns: none
+; Requires: NUL terminator at end of string
+; Clobbers: none
+print_string proc
+	push	ax
+	push	si
+	
+	mov	si, dx
+ps_loop:
+	lodsb
+	cmp	al, 0
+	je	ps_done
+	mov	ah, 0eh
+	int	10h
+	jmp	ps_loop
+ps_done:
+
+	pop	si
+	pop	ax
+	ret
+print_string endp
 
 main proc
 	mov  ax, @data			; @data is the data segment that DOS sets up.
@@ -69,8 +93,14 @@ main proc
 	
 	; Task Stacks will start at address something
 	
+	; Hello world
+	mov dx, OFFSET message 
+	call print_string
+	
 	
 	;exit
+	mov ah, 4Ch
+	int 21h
 main endp
 
 end main
