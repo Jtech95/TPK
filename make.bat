55d91182f0858fa482912bae3a34970547c1dc40
JSM <jtech1995@gmail.com>
2015-12-09 08:05:46 -0500

    general changes

--- make.bat
+++ make.bat
@@ -8,6 +8,11 @@ rem Assemble/link bootloader into flat binary file (not an EXE)
 %ASM% -3 -Fl -Fo mbr.obj mbr.asm || exit /b 1
 %LINK% format raw bin option quiet,map file mbr.obj name mbr.bin || exit /b 1
 
+rem Assemble/link TPK into flat binary file (not an EXE)
+%ASM% -3 -Fl -Fo tpk.obj tpk.asm || exit /b 1
+%LINK% format raw bin option quiet,map file tpk.obj name tpk.bin || exit /b 1
+
 rem Generate floppy disk image with MBR.BIN as the first 512-byte sector
 copy blank_floppy.img boot_floppy.img || exit /b 1
 %DD% bs=512 if=mbr.bin skip=62 of=boot_floppy.img conv=notrunc || exit /b 1
+%DD% bs=512 if=tpk.bin seek=1 of=boot_floppy.img conv=notrunc || exit /b 1
